c31149d270181a760a4225693a5b2256f2d93e74
Javier Rivera <javier.rivera52630@gmail.com>
2023-05-01 17:31:23 -0600

    convert

---
+++ convert.py
@@ -0,0 +1,28 @@
+import csv
+from ldif import LDIFWriter
+
+# Abre el archivo CSV para lectura y el archivo LDIF para escritura
+with open('contactos.csv', newline='', encoding='utf-8') as csv_file, \
+        open('contactos.ldif', mode='wb') as ldif_file:
+    # Crea un objeto LDIFWriter para escribir los datos en el archivo LDIF
+    ldif_writer = LDIFWriter(ldif_file)
+
+    # Lee los datos del archivo CSV y convierte cada fila a un registro LDIF
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        dn = 'cn={},ou=People,dc=adimacon,dc=com'.format(row[1])
+        attrs = {
+            'objectClass': ['top', 'person', 'organizationalPerson', 'inetOrgPerson'],
+            'cn': [str(row[0])],
+            'givenName': [str(row[0])],
+            'sn': [str(row[2])],
+            'mail': [str(row[8])],
+            'telephoneNumber': [str(row[15])],
+        }
+        # Convierte dn a una cadena de caracteres y escribe el registro en el archivo LDIF
+        ldif_writer.unparse(dn, attrs)
+        ldif_file.write(b'\n')
+
+print('Conversión de CSV a LDIF finalizada')
+
+
